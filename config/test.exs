35be054d16f035a8fa332f903736fdb3f0a9a430
gustavodm <gustadema@gmail.com>
2026-03-02 22:44:09 -0300

    fix: Fixed database config for user & password in test.exs

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :globaltask, Globaltask.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "globaltask",
+  password: "globaltask",
   hostname: "localhost",
   database: "globaltask_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
